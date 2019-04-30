038c21e06be0f7e5ce1cb6c30ebeb1e347709362
Hank <denghliu@126.com>
2019-04-30 09:30:20 +0800

    正式版本

--- src/providers/soap-api/soap-api.ts
+++ src/providers/soap-api/soap-api.ts
@@ -12,7 +12,7 @@ import { ApiProvider } from '../api/api';
 @Injectable()
 export class SoapApiProvider {
 
-  version:string = '1.0.0(2019041701)';
+  version:string = '1.0.0(2019043001)';
 
   HOSPITALCODE:string = '03013';
 
